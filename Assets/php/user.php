fb4e9f35e5cd833ffc5c4b815b29d69f2a97520f
yamato2199 <sonic19951220@hotmail.com>
2017-08-21 19:01:48 +1000

    users

--- Assets/php/user.php
+++ Assets/php/user.php
@@ -6,11 +6,13 @@ class User {
 	protected $email;
 	protected $password;
 	protected $type;
+	protected $username;
+	protected $gender;
 	
-	
-	public function getName()
+	public function getName($firstName,$lastName)
 	{
-	    return $this->name;
+		return $this->firstName;
+		return $this->lastName;
 	}
 	
 	public function setName($firstName,$lastName)
@@ -19,6 +21,64 @@ class User {
 		$this->lastName = $lastName;
 	}
 	
+	public function getUid($uid)
+	{
+		return $this->uid;
+	}
 
+	public function setUid($uid)
+	{
+		$this->uid = $uid;
+	}
+
+	public function getEmail($email)
+	{
+		return $this->email;
+	}
+
+	public function setEmail($email)
+	{
+		$this->email = $email;
+	}
+
+	public function getPassword($password)
+	{
+		return $this->password;
+	}
+
+	public function setPassword($password)
+	{
+		$this->password = $password;
+	}
+
+	public function getType($type)
+	{
+		return $this->type;
+	}
+
+	public function setType($type)
+	{
+		$this->type = $type;
+	}
+
+	public function getUsername($username)
+	{
+		return $this->username;
+	}
+
+	public function setUsername($username)
+	{
+		$this->username = $username;
+	}
+
+	public function getGender($gender)
+	{
+		return $this->gender;
+	}
+
+	public function setGender($gender)
+	{
+		$this->gender = $gender;
+	}
 }
 ?>
