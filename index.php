a07fea2b23cde0635f823d087830b031fe3a7300
Le Cai <episodes27@gmail.com>
2017-08-28 12:45:02 +1000

    修正了主页banner文字阴影CSS

--- index.php
+++ index.php
@@ -74,7 +74,7 @@
     
 	<div class="container-fluid" id="bk">
 	
-		<div class="container" style="margin-top: 35px;">
+		<div class="container">
 				
 				<div class="row">
 			
@@ -82,7 +82,7 @@
 		<div class="col-md-10 text-center" style="margin-left: auto; margin-right: auto; margin-top: 100px; margin-bottom: 120px;">
 		
 				<h1 class="text-center">XXXXXX</h1>
-				<form style="margin-top: 50px; margin-left: 20px; margin-right: 20px; ">
+				<form style="margin-top: 50px; margin-left: 20px; margin-right: 20px; " id="form_banner">
 				 <div class="row">
         <div class="col-md-2" style="margin: 0px; padding: 0px; margin-top: 5px;">
          	  <select class="form-control">
