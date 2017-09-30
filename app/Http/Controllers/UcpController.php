419c5ce2150efc6b151a818b0617a7652794084e
Epsiodes <episodes27@gmail.com>
2017-10-01 03:42:23 +1100

    [Extra] Add the dymic chart
    添加了图标在用户UCP首页

--- app/Http/Controllers/UcpController.php
+++ app/Http/Controllers/UcpController.php
@@ -1,15 +1,16 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use DB;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Auth;
 class UcpController extends Controller
 {
     //
     public function index()
     {
         return view('ucp/index');
+        //return DB::table('transactions')->where('user_id',Auth::user()->id)->groupBy('shop_name')->distinct()->get();
     }
 
     public function test()
